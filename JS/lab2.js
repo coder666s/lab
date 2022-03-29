b541dddd1f06756c018419201913e3458dedaa4f
coder666s <91024517+coder666s@users.noreply.github.com>
2022-03-29 13:01:17 +0300

    29.03.22
    1 и 2 номер лабораторной 2

---
+++ JS/lab2.js
@@ -0,0 +1,27 @@
+
+/*
+console.log("Номер 1");
+
+number_1 = prompt("Номер 1, Введите число для модуля: ");
+if(number_1<0)
+{
+ number_1 = parseInt(-number_1);
+ console.log("Модуль = "+number_1);
+}
+else
+{
+    console.log("Модуль = "+number_1);
+}
+
+console.log("Номер 2");
+
+word = prompt("Введите слово: ");
+console.log(word === word.split('').reverse().join(''));
+*/
+
+console.log("Номер 3");
+
+matrix1 = [[2,2][1,2]];
+matrix1 = [[1,4][2,5]];
+
+if()
