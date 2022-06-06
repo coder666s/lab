b007df5fd8f710cb53438c03563164631621c3ca
coder666s <91024517+coder666s@users.noreply.github.com>
2022-06-06 17:01:49 +0300

    111
    111

--- JS/lab3.js
+++ JS/lab3.js
@@ -5,9 +5,19 @@ function closeForm() {
   document.getElementById("myForm").style.display = "none";
 }
 
-function submit(event) {
-  let form = document.querySelector('myForm');
-  let formData = new FormData(form);
-  console.log(Array.from(formData));
-}
+
+function submit() {
+
+const name1 = document.querySelector("div.user-panel.main input[name='name']"), //получаем поле name
+//phone = form.querySelector('[name="phone"]'), //получаем поле age
+   // terms = form.querySelector('[name="terms"]'), //получаем поле terms
+   // plan = form.querySelector('[name="plan"]'); //получаем поле plan
+
+languages = [
   
+  { форма: "Имя" , анкета: name1},
+ // { name: phone, fileExtension: ".ts" },
+  //{ name: "CoffeeScript", fileExtension: ".coffee" }
+];
+console.table(languages);
+}
